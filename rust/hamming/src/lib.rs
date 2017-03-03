9f6b91360a04611974f9d8e562351465da214d61
NucsuM <nucsum@mailbox.org>
2017-03-03 22:53:38 +0100

    add rust-raindrops

---
+++ rust/hamming/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn distance(seqA:String, seqB:String) -> i32 {
+    if seqA.len() == seqB.len(){
+        //let array: [i32; 3] = [1, 2, 3];
+        println!("gleich");
+    }
+    else {
+        println!("ungleich");
+    }
+
+}
---
+++ rust/hamming/target/debug/hamming.d
@@ -0,0 +1,5 @@
+C:\Users\ns\dev\exercism\rust\hamming\target\debug\libhamming.rlib: src\lib.rs
+
+C:\Users\ns\dev\exercism\rust\hamming\target\debug\hamming.d: src\lib.rs
+
+src\lib.rs:
